11df7d5a4ccb2f2dc42bc2ec0ac74fc8fd55f8e8
Sobeshchanskiy Dmitriy Mikhaylovich <dsobeshchanskiy@gmail.com>
2019-12-15 18:51:06 +0200

    Employee list component events

--- business-optimisation-app/src/app/dsob/employee/employee-list/employee-list.component.ts
+++ business-optimisation-app/src/app/dsob/employee/employee-list/employee-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { EmployeeService } from '../services/employee.service';
 import { Employee } from 'src/app/models/HR/Employee';
 
@@ -8,6 +8,11 @@ import { Employee } from 'src/app/models/HR/Employee';
   styleUrls: ['./employee-list.component.css']
 })
 export class EmployeeListComponent implements OnInit {
+
+  @Output() currentEmpChanged = new EventEmitter<number>();
+  @Output() addNewRequested = new EventEmitter();
+  @Output() deleteRequested = new EventEmitter<number>();
+  @Output() editRequested = new EventEmitter<number>();
 
   employees = new Array<Employee>();
 
@@ -27,12 +32,25 @@ export class EmployeeListComponent implements OnInit {
     this.employees = this.employeeService.employees;
   }
 
-  onRowClick(employee: Employee) {
-    this.currentEmployee = employee;
-  }
-
   isSelected(employee: Employee): boolean {
     return this.currentEmployee && this.currentEmployee.id === employee.id;
+  }
+
+  onRowClick(employee: Employee) {
+    this.currentEmployee = employee;
+    this.currentEmpChanged.emit(this.currentEmployee.id);
+  }
+
+  onAddClick(event: MouseEvent) {
+    this.addNewRequested.emit();
+  }
+
+  onEditClick(event: MouseEvent) {
+    this.editRequested.emit(this.currentEmployee.id);
+  }
+
+  onDeleteClick(event: MouseEvent) {
+    this.deleteRequested.emit(this.currentEmployee.id);
   }
 
 }
