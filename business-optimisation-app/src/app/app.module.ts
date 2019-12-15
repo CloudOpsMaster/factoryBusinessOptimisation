790e255eb91b73e91799f0ca6fc11da4678767dc
yanabdev <heureuse564@gmail.com>
2019-12-15 11:33:36 +0200

    added create material component to AppModule declarations

--- business-optimisation-app/src/app/app.module.ts
+++ business-optimisation-app/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EmployeeManagerComponent } from './dsob/employee/employee-manager/employee-manager.component';
 import { HomeComponent } from './home/home/home.component';
+import { CreateMaterialFormComponent } from './yulia/create-material-form/create-material-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    EmployeeManagerComponent
+    EmployeeManagerComponent,
+    CreateMaterialFormComponent
   ],
   imports: [
     BrowserModule,
