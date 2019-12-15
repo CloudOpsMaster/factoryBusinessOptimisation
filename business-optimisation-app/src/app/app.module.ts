89f3182ff3ec96e968fe03236d65757cffaf5a01
Sobeshchanskiy Dmitriy Mikhaylovich <dsobeshchanskiy@gmail.com>
2019-12-15 10:48:47 +0200

    Routing and menu (initial)

--- business-optimisation-app/src/app/app.module.ts
+++ business-optimisation-app/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EmployeeManagerComponent } from './dsob/employee/employee-manager/employee-manager.component';
+import { HomeComponent } from './home/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    EmployeeManagerComponent
   ],
   imports: [
     BrowserModule,
