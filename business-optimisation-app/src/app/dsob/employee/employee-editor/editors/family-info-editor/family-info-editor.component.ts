1aa2362e40db363e4784a341ebc87daf5afca50f
Sobeshchanskiy Dmitriy Mikhaylovich <dsobeshchanskiy@gmail.com>
2020-01-07 16:39:28 +0200

    HR: family info editor - add children

--- business-optimisation-app/src/app/dsob/employee/employee-editor/editors/family-info-editor/family-info-editor.component.ts
+++ business-optimisation-app/src/app/dsob/employee/employee-editor/editors/family-info-editor/family-info-editor.component.ts
@@ -1,6 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { BaseEditor } from '../base-editor';
-import { FamilyInfo } from 'src/app/models/HR/FamilyInfo';
+import { FamilyInfo, Child } from 'src/app/models/HR/FamilyInfo';
 
 @Component({
   selector: 'app-family-info-editor',
@@ -11,9 +11,61 @@ export class FamilyInfoEditorComponent extends BaseEditor {
 
   @Input() family: FamilyInfo;
 
+  get childCount(): string {
+    let response = 'нет';
+    if (this.family && this.family.children.length > 0) {
+      response = this.family.children.length.toString();
+    }
+    return response;
+  }
+
+  get addNewChildDisabled(): boolean {
+    return !this.editMode || !this.newChild.dob;
+  }
+
+  newChild = new Child();
+  editChildId = -1;
+
+  editCaptionFor(child: Child): string {
+    if (child.id === this.editChildId) {
+      return 'Cancel';
+    } else {
+      return 'Edit';
+    }
+  }
+
+  isDobReadonlyForChild(child): boolean {
+    return child.id !== this.editChildId;
+  }
+
+  onAddChild() {
+    this.assignIdForNewChild();
+    this.family.children.push(this.newChild.clone());
+    this.newChild = new Child();
+  }
+
+  onDeleteClick(child: Child) {
+    this.family.children = this.family.children.filter(c => c.id !== child.id);
+  }
+
+  onEditClick(child: Child) {
+    if (child.id === this.editChildId) {
+      this.editChildId = -1;
+    } else {
+      this.editChildId = child.id;
+    }
+  }
+
   constructor() {
     super();
   }
 
+  private assignIdForNewChild() {
+    if (this.family.children.length > 0) {
+      this.newChild.id = this.family.children[this.family.children.length].id + 1;
+    } else {
+      this.newChild.id = 1;
+    }
+  }
 
 }
