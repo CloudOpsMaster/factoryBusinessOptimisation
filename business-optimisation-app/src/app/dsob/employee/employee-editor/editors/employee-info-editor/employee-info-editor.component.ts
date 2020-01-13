7a3c87ff535d67568e67f03115165d209197e9ba
Dmytro Sobeshchanskyy <dsob@isd.dp.ua>
2020-01-13 20:14:46 +0200

    HR: optionally show employee id

--- business-optimisation-app/src/app/dsob/employee/employee-editor/editors/employee-info-editor/employee-info-editor.component.ts
+++ business-optimisation-app/src/app/dsob/employee/employee-editor/editors/employee-info-editor/employee-info-editor.component.ts
@@ -1,21 +1,22 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, Output } from '@angular/core';
 import { EmployeeMainInfo } from '../../../EmployeeInfo';
 import { BaseEditor } from '../base-editor';
 
 @Component({
-  selector: 'app-employee-info-editor',
-  templateUrl: './employee-info-editor.component.html',
-  styleUrls: ['./employee-info-editor.component.css']
+   selector: 'app-employee-info-editor',
+   templateUrl: './employee-info-editor.component.html',
+   styleUrls: ['./employee-info-editor.component.css']
 })
 export class EmployeeInfoEditorComponent extends BaseEditor {
 
-  @Input() mainInfo: EmployeeMainInfo;
+   @Input() mainInfo: EmployeeMainInfo;
+   @Input() showId = false;
 
-  onModelChange(path: any) {
-     console.log('path:', path);
-  }
-  constructor() {
-    super();
-  }
+   onModelChange(path: any) {
+      console.log('path:', path);
+   }
+   constructor() {
+      super();
+   }
 
 }
