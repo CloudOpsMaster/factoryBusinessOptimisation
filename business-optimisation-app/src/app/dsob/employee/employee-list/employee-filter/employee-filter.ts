9ce9aea275a986c622047c80a127e4e5f2828727
Dmytro Sobeshchanskyy <dsob@isd.dp.ua>
2020-01-13 20:26:46 +0200

    HR: extend employee search

--- business-optimisation-app/src/app/dsob/employee/employee-list/employee-filter/employee-filter.ts
+++ business-optimisation-app/src/app/dsob/employee/employee-list/employee-filter/employee-filter.ts
@@ -3,6 +3,9 @@ export class EmployeeFilter {
     firstName: string;
     secondName: string;
     patronymic: string;
+    id: string;
+    passport: string;
+    taxNumber: string;
     // TODO: provide more filters
 }
 
--- business-optimisation-app/src/app/dsob/employee/employee-list/employee-filter/employee-filtrator.ts
+++ business-optimisation-app/src/app/dsob/employee/employee-list/employee-filter/employee-filtrator.ts
@@ -2,21 +2,30 @@ import { EmployeeInfo } from '../../EmployeeInfo';
 import { EmployeeFilter } from './employee-filter';
 
 export class EmployeeFiltrator {
-    public static GetFilteredEmployees(employees: EmployeeInfo[], filter: EmployeeFilter): EmployeeInfo[] {
-        let filteredEmployees = employees.filter(e => e);
-        if (!filter) {
-            return filteredEmployees;
-        }
-        if (filter.firstName && filter.firstName.length > 0) {
-            filteredEmployees = employees.filter(e => e.mainInfo.firstName.startsWith(filter.firstName));
-        }
-        if (filter.secondName && filter.secondName.length > 0) {
-            filteredEmployees = filteredEmployees.filter(e => e.mainInfo.secondName.startsWith(filter.secondName));
-        }
-        if (filter.patronymic && filter.patronymic.length > 0) {
-            filteredEmployees = filteredEmployees.filter(e => e.mainInfo.patronymic.startsWith(filter.patronymic));
-        }
+   public static GetFilteredEmployees(employees: EmployeeInfo[], filter: EmployeeFilter): EmployeeInfo[] {
+      let filteredEmployees = employees.filter(e => e);
+      if (!filter) {
+         return filteredEmployees;
+      }
+      if (filter.firstName && filter.firstName.length > 0) {
+         filteredEmployees = employees.filter(e => e.mainInfo.firstName.startsWith(filter.firstName));
+      }
+      if (filter.secondName && filter.secondName.length > 0) {
+         filteredEmployees = filteredEmployees.filter(e => e.mainInfo.secondName.startsWith(filter.secondName));
+      }
+      if (filter.patronymic && filter.patronymic.length > 0) {
+         filteredEmployees = filteredEmployees.filter(e => e.mainInfo.patronymic.startsWith(filter.patronymic));
+      }
+      if (filter.patronymic && filter.patronymic.length > 0) {
+         filteredEmployees = filteredEmployees.filter(e => e.mainInfo.patronymic.startsWith(filter.patronymic));
+      }
+      if (filter.id && filter.id.length > 0) {
+         filteredEmployees = filteredEmployees.filter(e => e.mainInfo.id.toString().startsWith(filter.id));
+      }
+      if (filter.taxNumber && filter.taxNumber.length > 0) {
+         filteredEmployees = filteredEmployees.filter(e => e.document.taxNumber.toString().startsWith(filter.taxNumber));
+      }
 
-        return filteredEmployees;
-    }
+      return filteredEmployees;
+   }
 }
