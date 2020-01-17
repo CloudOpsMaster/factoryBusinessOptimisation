c1328ede7595762abeceaff67026947e9e06b5a6
Dmytro Sobeshchanskyy <dsob@isd.dp.ua>
2020-01-17 20:46:12 +0200

    Common: buttons namespace correction

--- business-optimisation-app/src/app/common/common.module.ts
+++ business-optimisation-app/src/app/common/common.module.ts
@@ -4,12 +4,12 @@ import { ComboBoxComponent } from './ui/combo-box/combo-box.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from '../app-routing.module';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { SaveButtonComponent } from './ui/combo-box/save-button/save-button.component';
-import { EditButtonComponent } from './ui/combo-box/edit-button/edit-button.component';
-import { DeleteButtonComponent } from './ui/combo-box/delete-button/delete-button.component';
-import { AddButtonComponent } from './ui/combo-box/add-button/add-button.component';
-import { ResetButtonComponent } from './ui/combo-box/reset-button/reset-button.component';
-import { FilterButtonComponent } from './ui/combo-box/filter-button/filter-button.component';
+import { SaveButtonComponent } from './ui/buttons/save-button/save-button.component';
+import { EditButtonComponent } from './ui/buttons/edit-button/edit-button.component';
+import { DeleteButtonComponent } from './ui/buttons/delete-button/delete-button.component';
+import { AddButtonComponent } from './ui/buttons/add-button/add-button.component';
+import { ResetButtonComponent } from './ui/buttons/reset-button/reset-button.component';
+import { FilterButtonComponent } from './ui/buttons/filter-button/filter-button.component';
 
 
 @NgModule({
