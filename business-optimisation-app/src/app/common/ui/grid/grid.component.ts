0d8baaab5183c115c87634c51f349f8352fd7848
Dmytro Sobeshchanskyy <dsob@isd.dp.ua>
2020-01-20 16:01:43 +0200

    Common: add grid documentation

--- business-optimisation-app/src/app/common/ui/grid/grid.component.ts
+++ business-optimisation-app/src/app/common/ui/grid/grid.component.ts
@@ -10,13 +10,21 @@ import { GridOptions } from './model/grid-options';
 })
 export class GridComponent implements OnInit {
 
+   /** data source for grid. Just array of any objects */
    @Input() dataSource: Array<any>;
+   /** array of columns to display on grid */
    @Input() columns: Array<GridColumn>;
+   /** grid customization options options. Optional. */
    @Input() options: GridOptions = new GridOptions();
+   /** is grid locked or not (can accept click or not). Default value is false */
    @Input() locked = false;
+   /** hide / show selection for currently selected row. Default value is false */
    @Input() hideSelection = false;
-   @Input() focusEntityId: any; // ???
+   /** value of tracktionField property for currently selected object/row  */
+   @Input() focusEntityId: any;
+   /** name of object's field for to track identity */
    @Input() tracktionField: string;
+   /** Event that fires on row selection changed. Uses selected object as argument */
    @Output() currentRowChanged = new EventEmitter<any>();
 
    constructor() { }
