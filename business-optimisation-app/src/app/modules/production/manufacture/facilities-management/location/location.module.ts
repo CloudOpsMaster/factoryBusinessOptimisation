35f87964d46bbb1c5acc77956ac2a9161f8b4c0e
MashaBut <mashutkabut99@gmail.com>
2020-03-07 23:45:55 +0200

    fix conflict

--- business-optimisation-app/src/app/modules/production/manufacture/facilities-management/location/location.module.ts
+++ business-optimisation-app/src/app/modules/production/manufacture/facilities-management/location/location.module.ts
@@ -5,10 +5,7 @@ import { LocationRoutingModule } from './location-routing.module';
 import { LocationComponent } from './location.component';
 import { LocalizationModule } from 'angular-l10n';
 import { LocationFormComponent } from './location-form/location-form.component';
-import { CommonAppModule } from 'src/app/common/common.module';
-import { HomeComponent } from 'src/app/chapters/home/home.component';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [ LocationComponent, LocationFormComponent],
