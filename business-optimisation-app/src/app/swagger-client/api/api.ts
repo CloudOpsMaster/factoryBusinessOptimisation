91a92b4e028c83b0d95f6fe4eb561a25038115f9
MashaBut <mashutkabut99@gmail.com>
2020-03-07 23:28:56 +0200

    add swagger

---
+++ business-optimisation-app/src/app/swagger-client/api/api.ts
@@ -0,0 +1,3 @@
+export * from './facilitiesManagement.service';
+import { FacilitiesManagementService } from './facilitiesManagement.service';
+export const APIS = [FacilitiesManagementService];
---
+++ business-optimisation-app/src/app/swagger-client/model/models.ts
@@ -0,0 +1,3 @@
+export * from './address';
+export * from './location';
+export * from './workPlace';
